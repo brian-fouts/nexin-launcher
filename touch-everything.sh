95961a41e21975c7068822ed51f932a1065634a1
Brian Fouts <brian@brian-fouts.com>
2026-03-09 22:53:34 +0100

    makeeverything build

---
+++ touch-everything.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+date > discord-bot/README.md
+date > matchmaker-frontend/README.md
+date > matchmaker-backend/README.md
+date > game-frontend/README.md
+date > game-backend/README.md
+date > tower-defense-backend/README.md
+date > tower-defense-frontend/README.md
